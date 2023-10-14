ffe90efa935c0353ca7f545a7bc08f380a3f2d17
Rajat Verma <rajat311201@gmail.com>
2023-10-15 03:50:31 +0530

    cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ const createSendToken = (user, statusCode, res) => {
         expires: cookieExpiry,
         httpOnly: true,
     };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
     res.cookie('jwt', token, cookieOptions);
 
