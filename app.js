8a440caeef2722b7679117bcf79a43c9ffefdfa5
Rajat Verma <rajat311201@gmail.com>
2023-11-07 03:01:54 +0530

    cookie fix2:

--- app.js
+++ app.js
@@ -66,7 +66,16 @@ const corsOptions = {
     credentials: true,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header(
+        'Access-Control-Allow-Origin',
+        'https://adventour-react.vercel.app',
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // Development logging
 // if (process.env.NODE_ENV === 'development') {
